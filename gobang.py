f5d24df9adef0741ead6e10fbcde443554f44136
jstenger <jake.stenger.19@bishops.com>
2020-05-21 13:12:03 -0700

    Fixed shallow copy bug, added ASCII board printout

--- gobang.py
+++ gobang.py
@@ -1,17 +1,17 @@
 import argparse
-import operator
+import pprint
 
 import random
 
 
-def human(black_board, white_board):
+def human(self_board, opponent_board):
     while True:
-        print("Make a move (<letter><number>)")
+        print("Make a move")
         movestr = input()
-        if len(movestr) == 2 and movestr[0].isalpha() and movestr[1].isdigit():
-            x_pos, y_pos = ord(movestr[0]) - ord('A'), int(movestr[1])
-            if x_pos < len(black_board) and y_pos <= len(black_board):
-                if not black_board[x_pos][y_pos] or white_board[x_pos][y_pos]:
+        if len(movestr) < 4 and movestr[0].isalpha() and movestr[1:].isdigit():
+            x_pos, y_pos = ord(movestr[0].lower()) - ord('a'), int(movestr[1:]) - 1
+            if x_pos in range(len(self_board)) and y_pos in range(len(self_board)):
+                if not (self_board[x_pos][y_pos] or opponent_board[x_pos][y_pos]):
                     break
                 else:
                     print("That tile is occupied")
@@ -21,15 +21,20 @@ def human(black_board, white_board):
             print("Invalid input format")
 
     print("Move played: " + str(movestr))
-    return x_pos, y_pos
+    self_board[x_pos][y_pos] = True
 
-def ai(black_board, white_board):
-    x, y = random.randint(0, len(black_board)), random.randint(0, len(black_board))
-    while black_board[x][x] or white_board[x][x]:
-        x, y = random.randint(0, len(black_board)), random.randint(0, len(black_board))
+    return
 
-    print("Move played: " + chr(ord('A') + x) + str(y))
-    return x, y
+
+def ai(self_board, opponent_board):
+    x, y = random.randint(0, len(self_board) - 1), random.randint(0, len(self_board) - 1)
+    while self_board[x][y] or opponent_board[x][y]:
+        x, y = random.randint(0, len(self_board) - 1), random.randint(0, len(self_board) - 1)
+
+    print("Move played: " + chr(ord('A') + x) + str(y + 1))
+    self_board[x][y] = True
+    return
+
 
 def win(black_board, white_board):
     for board in black_board, white_board:
@@ -58,6 +63,7 @@ def win(black_board, white_board):
 
     return False
 
+
 def board_full(black_board, white_board):
     return all([all([any(pair) for pair in zip(column[0], column[1])]) for column in zip(black_board, white_board)])
 
@@ -66,17 +72,31 @@ def game_over(black_board, white_board):
     return win(black_board, white_board) or board_full(black_board, white_board)
 
 
+def print_board(black_board, white_board):
+    print("   " + " ".join([chr(l + ord("A")) for l in range(len(black_board))]))
+    for row in range(len(black_board)):
+        row_string = " " + str(row + 1) if row < 9 else str(row + 1)
+        for col in range(len(black_board)):
+            if black_board[col][row]:
+                row_string += "|" + "B"
+            elif white_board[col][row]:
+                row_string += "|" + "W"
+            else:
+                row_string += "|" + " "
+        row_string += "|"
+        print(row_string)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", default=11)
     parser.add_argument("-l", action="store_true")
     args = parser.parse_args()
-    board_size = args.n
+    board_size = int(args.n)
     human_first = not args.l
 
-
-    black_board = [[False for i in range(board_size)] for i in range(board_size)]
-    white_board = [[False for i in range(board_size)] for i in range(board_size)]
+    black_board = [l[:] for l in [[False]*board_size]*board_size]
+    white_board = [l[:] for l in [[False]*board_size]*board_size]
 
     if human_first:
         black_player = human
@@ -88,17 +108,15 @@ def main():
     black_turn = True
     while not game_over(black_board, white_board):
         if black_turn:
-            move = black_player(black_board, white_board)
-            black_board[move[0]][move[1]] = True
+            black_player(black_board, white_board)
 
         else:
-            move = white_player(black_board, white_board)
-            white_board[move[0]][move[1]] = True
+            white_player(white_board, black_board)
 
+        print_board(black_board, white_board)
         black_turn = not black_turn
 
-
-    if(win(black_board, white_board)):
+    if win(black_board, white_board):
         if black_turn:
             print("White wins!")
         else:
